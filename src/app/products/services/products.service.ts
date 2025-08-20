1e0717f3b2caf0a1ce16ddf12f83d5f9ed049ff2
Billy Alvear <alvearbilly@gmail.com>
2025-08-19 22:04:14 -0500

    paginationAndCache

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -4,7 +4,7 @@ import {
   Product,
   ProductsResponse,
 } from '@products/interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -19,8 +19,19 @@ interface Options {
 export class ProductsService {
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
+
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+
+    // revisar entradas en el cache
+    // console.log(this.productsCache.entries());
+
+    const key = `${limit}-${offset}-${gender}`; //9-0-''
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
 
     return this.http
       .get<ProductsResponse>(`${baseUrl}/products`, {
@@ -30,12 +41,20 @@ export class ProductsService {
           gender,
         },
       })
-      .pipe(tap((resp) => console.log(resp)));
+      .pipe(
+        tap((resp) => console.log(resp)),
+        tap((resp) => this.productsCache.set(key, resp))
+      );
   }
 
   getProductsByIdSlug(idSlug: string): Observable<Product> {
-    return this.http
-      .get<Product>(`${baseUrl}/products/${idSlug}`)
-      .pipe(tap((resp) => console.log(resp)));
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      tap((resp) => console.log(resp)),
+      tap((resp) => this.productCache.set(idSlug, resp))
+    );
   }
 }
--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,22 +2,36 @@ import { Component, inject, signal } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ProductsResponse } from '@products/interfaces/product.interface';
-import { map } from 'rxjs';
-// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
   produtcsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page))
+  //   ),
+  //   {
+  //     initialValue: 1,
+  //   }
+  // );
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
     stream: ({ params }) => {
-      return this.produtcsService.getProducts({});
+      return this.produtcsService.getProducts({
+        offset: params.page * 9,
+      });
     },
   });
 }
